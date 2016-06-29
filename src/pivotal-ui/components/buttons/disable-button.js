98b2177197fe3223ecf53377b3033e23f363b814
Jeffrey Lembeck <jlembeck@gmail.com>
2016-06-29 14:55:09 -0700

    fix(buttons): simplify button disabling and make it a bit more specific

--- src/pivotal-ui/components/buttons/disable-button.js
+++ src/pivotal-ui/components/buttons/disable-button.js
@@ -1,14 +1,22 @@
 var button;
 var handleInputChangeForDisable = function handleInputChangeForDiable(e){
-  if(e.detail.currentValue === button.getAttribute('data-username')){
-    button.setAttribute("disabled", "disabled");
-  } else {
-    button.removeAttribute("disabled");
+  if(!button){
+    return;
   }
+  var input = e && e.target;
+
+  if (input && input.name === 'orgScope') {
+    if (input.value === button.getAttribute("data-username")) {
+      button.setAttribute("disabled", "disabled");
+    } else {
+      button.removeAttribute("disabled");
+    }
+  }
+
 };
 
 document.addEventListener('DOMContentLoaded', function(){
-  button = document.querySelector("[data-button-listening='FormInput:input'][data-button-type='disable']");
+  button = document.querySelector("[data-button-type='disable-on-matching-username']");
+  document.addEventListener('input', handleInputChangeForDisable, false);
 });
 
-document.addEventListener('FormInput:input', handleInputChangeForDisable, false);
