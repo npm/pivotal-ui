f28cbaf85f8f3105fbb3963b5448af5fe9632b9b
Jeffrey Lembeck <jlembeck@gmail.com>
2016-07-13 11:44:27 -0700

    fix(forms): Link updater handles own errors
    When using the link updater, we want to handle errors within the link
    updater module itself. This incurs tech debt for us, as it requires
    changing the way errors look in multiple places now.
    
    There are potential fixes for that, though, one of which is to make
    adding and removing errors a function of the validator itself.
    
    [Fixes #126237181]

--- src/pivotal-ui/components/forms/link-updater.js
+++ src/pivotal-ui/components/forms/link-updater.js
@@ -1,6 +1,8 @@
 var $ = require('jquery');
 var isInvalid = require('npm-user-validate').username;
 
+var errorClass = "has-error";
+
 var LinkUpdater = function(el){
   this.el = $(el);
   this.updater = this.el.find(".link-updater");
@@ -12,16 +14,48 @@ var LinkUpdater = function(el){
 
   this.pathDisplay = this.el.find("p strong");
   this.demoValue = this.input.attr("placeholder") || "username";
+  this.currentValue = "";
 };
 
 LinkUpdater.prototype.updateValue = function(inputValue){
-  if(inputValue === "") {
-    this.pathDisplay.text(this.demoValue);
-  } else if (!this.input[0].checkValidity() || isInvalid(inputValue)){
-    //noop
+  var err = isInvalid(inputValue);
+  if(err) {
+    inputValue = this.currentValue;
+    this.input.val(this.currentValue);
+    this.showError(err.message);
+  } else if(!this.input[0].checkValidity()) {
+    this.showError(this.input[0].validationMessage);
   } else {
-    this.pathDisplay.text(inputValue);
+    this.hideError();
   }
+
+  var pathVal = inputValue;
+
+  if(inputValue === ""){
+    pathVal = this.demoValue;
+  }
+
+  this.currentValue = inputValue;
+  this.pathDisplay.text(pathVal);
+};
+
+LinkUpdater.prototype.showError = function(msg){
+  var err = this.updater.find("." + errorClass);
+
+  if(!err.length) {
+    err = $("<span class='help-block " + errorClass + "'>" + msg + "</span>");
+
+    this.el.addClass(errorClass);
+    this.updater.append(err);
+  } else {
+    err.text(msg);
+  }
+
+};
+
+LinkUpdater.prototype.hideError = function(){
+  this.el.removeClass(errorClass);
+  this.updater.find("." + errorClass).remove();
 };
 
 $(function(){
