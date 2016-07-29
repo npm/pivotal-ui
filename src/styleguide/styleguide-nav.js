dbc490090bdff9e7c4c686f0e7970ceee6bfa827
Aria Stewart <aredridel@dinhe.net>
2016-07-29 11:09:26 -0700

    Retain the current language selection across navigation

--- src/styleguide/styleguide-nav.js
+++ src/styleguide/styleguide-nav.js
@@ -28,26 +28,28 @@ class ComponentList extends React.Component {
 
 export class StyleguideNav extends React.Component {
   render() {
-    const {navTree} = this.props;
+    const {navTree, lang} = this.props;
 
     const navByLanguage = {
       CSS: reduce(values(navTree.CSS), (e, a) => Object.assign({}, e, a), {}),
       React: reduce(values(navTree.React), (e, a) => Object.assign({}, e, a), {})
     };
 
+    const cssActive = lang == 'css' ? 'active in' : '';
+    const reactActive = lang == 'react' ? 'active in' : '';
     return (
       <div className="tab-simple">
         <ul className="nav nav-tabs">
-          <li className="active"><a data-toggle="tab" href="#lang-css">CSS</a></li>
-          <li><a data-toggle="tab" href="#lang-react">React</a></li>
+          <li className={cssActive}><a data-toggle="tab" href="#lang-css">CSS</a></li>
+          <li className={reactActive}><a data-toggle="tab" href="#lang-react">React</a></li>
         </ul>
         <div className="tab-content">
-          <div className="tab-pane fade in active pan" id="lang-css">
+          <div className={`tab-pane fade in ${cssActive} pan`} id="lang-css">
             <ul className="list-unstyled mlxl">
               {lodash.map(navByLanguage.CSS, (v, k) => <li key={`component-CSS-${k}`}><a href={v}>{k}</a></li>)}
             </ul>
           </div>
-          <div className="tab-pane fade pan" id="lang-react">
+          <div className={`tab-pane fade ${reactActive} pan`} id="lang-react">
             <ul className="list-unstyled mlxl">
               {lodash.map(navByLanguage.React, (v, k) => <li key={`component-React-${k}`}><a href={v}>{k}</a></li>)}
             </ul>
